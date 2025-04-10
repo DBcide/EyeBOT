6a748e75a1e2c99224458d1b644188144e8c9434
DBcide <80953860+DBcide@users.noreply.github.com>
2025-04-10 11:54:52 +0200

    Update db.js

--- db.js
+++ db.js
@@ -8,14 +8,12 @@ const pool = mysql.createPool({
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     waitForConnections: true,
-    connectionLimit: 10, // Limite de connexions simultanées
+    connectionLimit: 10,
     queueLimit: 0,
     supportBigNumbers: true,
     bigNumberStrings: true
 });
 
-// Utiliser le pool pour la gestion des requêtes
 const promisePool = pool.promise();
 
-// Exporter le pool
 module.exports = { pool, promisePool };
