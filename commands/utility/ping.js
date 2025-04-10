93e9eda7e2011c11e0bb6f9a0263b81ed9b17ca4
DBcide <80953860+DBcide@users.noreply.github.com>
2025-04-10 12:00:08 +0200

    Update ping.js

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -21,27 +21,20 @@ module.exports = {
     async execute(interaction) {
         const { client, user } = interaction;
 
-        // Début de la mesure de latence
         const start = Date.now();
 
-        // Déférer la réponse (évite "InteractionAlreadyReplied")
         await interaction.deferReply({ flags: [MessageFlags.Ephemeral] });
 
-        // Calculer la latence du bot
         const latency = Date.now() - start;
 
-        // Récupérer la latence WebSocket de Discord
         let apiLatency = Math.round(client.ws.ping);
-        if (apiLatency === -1) apiLatency = "Unavailable"; else apiLatency = `${apiLatency}ms`; // Empêche d'afficher -1ms
+        if (apiLatency === -1) apiLatency = "Unavailable"; else apiLatency = `${apiLatency}ms`;
 
-        // Récupérer les infos du développeur
         const dev = await getUserInfo(client, dev_id);
 
-        // Vérifier si `dev` a bien été récupéré
         const footerText = dev ? `Dev by ${dev.username}` : `Dev information unavailable`;
         const footerIcon = dev ? dev.avatarURL : null;
 
-        // Création de l'embed avec la vraie latence
         const embed = new EmbedBuilder()
             .setColor('#00FF00')
             .setAuthor({ name: user.username, iconURL: user.displayAvatarURL({ dynamic: true, size: 512 }) })
@@ -50,7 +43,6 @@ module.exports = {
             .setFooter({ text: footerText, iconURL: footerIcon })
             .setTimestamp();
 
-        // Modifier la réponse initiale avec l'embed
         await interaction.editReply({ embeds: [embed] });
     },
 };
